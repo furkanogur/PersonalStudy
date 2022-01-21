6797710e3b9369a89a17a9dfe457d2b85ec6a301
furkanogur <furkan_ogur07@hotmail.com>
2022-01-21 17:38:20 +0300

    js-firstview

---
+++ javascript/javascript-firstView/script.js
@@ -0,0 +1,18 @@
+console.log("Merhaba");
+console.log(1234);
+console.log(true);
+console.log([1, 2, 3]);
+console.error("Hata oluştu");
+console.warn("uyarı");
+
+//yorum satırı
+
+/*
+çoklu yorum satırı
+çoklu yorum satırı
+çoklu yorum satırı
+çoklu yorum satırı
+çoklu yorum satırı
+*/
+
+console.clear();
