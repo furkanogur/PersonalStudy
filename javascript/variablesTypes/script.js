122ab624303541db839682186525b999b146cccb
furkanogur <furkan_ogur07@hotmail.com>
2022-01-22 10:01:40 +0300

    variablesTypes

---
+++ javascript/variablesTypes/script.js
@@ -0,0 +1,46 @@
+//string 
+
+let firstName = 'furkan';
+console.log(typeof firstName);
+
+
+//number 
+
+let age = 20;
+console.log(typeof age);
+let money = 100.5;
+console.log(typeof money);
+
+//Boolen
+let isActive = true;
+console.log(typeof isActive);
+
+//null
+let job = null;
+console.log(typeof job);
+
+//undefined
+let car;
+console.log(typeof car);
+
+//reference types - objects
+
+//Array
+let names = ['Ali', 'Ahmet', 'Can'];
+console.log(typeof names);
+
+// Object 
+
+let address = {
+    city: 'Antalya',
+    country: 'Turkey'
+}
+console.log(typeof address);
+
+//function
+
+var calculateAge = function() {
+    return 0;
+};
+
+console.log(typeof calculateAge)
