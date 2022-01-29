bd6ed6449fdea05740cd04c5570fd1dd60fa2dc0
furkanogur <furkan_ogur07@hotmail.com>
2022-01-29 09:47:49 +0300

    windowObject

---
+++ javascript/functions/windowObject/script.js
@@ -0,0 +1,47 @@
+var a = 10;
+let val;
+
+function abc() {
+    return 0;
+}
+
+
+val = window;
+
+
+//alert
+// alert('Merhaba')
+
+//prompt
+// var val = prompt('bir sayı giriniz');
+
+// confirm
+// val = confirm = confirm('emin misiniz?')
+
+// if (val) {
+//     console.log('ok')
+// } else {
+//     console.log('no')
+// }
+
+
+// scroll
+// val = window.scrollX;
+// val = window.scrollY;
+
+
+//location
+val = window.location;
+val = window.location.href;
+val = window.location.hostname;
+val = window.location.host;
+val = window.location.protocol;
+val = window.location.search;
+
+// window.location.href = 'http://furkanogur.com'
+// window.location.reload();
+
+
+//navigator
+
+console.log(val);
