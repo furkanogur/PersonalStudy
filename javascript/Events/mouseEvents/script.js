484fd519c852317c4c951b4ee695fcd48d2f30e3
furkanogur <furkan_ogur07@hotmail.com>
2022-02-02 17:03:46 +0300

    eventHandler

---
+++ javascript/Events/mouseEvents/script.js
@@ -0,0 +1,40 @@
+const btn = document.querySelector('#btnDeleteAll');
+const ul = document.querySelector('#task-list')
+
+//click
+// btn.addEventListener('click', eventHandler);
+// ul.addEventListener('click', eventHandler);
+
+
+//double click
+// btn.addEventListener('dblclick', eventHandler)
+
+
+// //mousedown
+// btn.addEventListener('mousedown', eventHandler);
+// //mouseup
+// btn.addEventListener('mouseup', eventHandler);
+
+// //mouseenter
+// ul.addEventListener('mouseenter', eventHandler);
+// //mouseleave
+// ul.addEventListener('mouseleave', eventHandler);
+
+// //mouseover
+// ul.addEventListener('mouseover', eventHandler);
+// //mouseout
+// ul.addEventListener('mouseout', eventHandler);
+
+
+//mouse move
+
+const h5 = document.querySelector('h5')
+ul.addEventListener('mousemove', eventHandler)
+
+function eventHandler(event) {
+    console.log(`event type : ${event.type}`);
+
+
+    h5.textContent = `Mouse X : ${event.offsetX} Mouse Y : ${event.offsetY}`;
+
+}
